f29fad3ec431673c8bb725a080bcc8ec5d1141e2
Esteban Kramer <Estebankramer@hotmail.com>
2018-08-19 18:54:38 -0300

    working version

--- ej1.c
+++ ej1.c
@@ -1,26 +1,30 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
+#include "ej1.h"
 
 static int pipeCtoP [2] = {-1,-1};
 static int pipePtoC [2] = {-1,-1};
 static int stdinFD = STDIN_FILENO;
 static int stdoutFD = STDOUT_FILENO;
 
+int bitParity(char * number) {
 
-int bitParity(int number) {
   int b = 1;
 
-  b = b ^ (number);
-  b = b ^ (number >> 1);
-  b = b ^ (number >> 2);
-  b = b ^ (number >> 4);
-  b = b ^ (number >> 5);
-  b = b ^ (number >> 6);
-  b = b ^ (number >> 7);
+  b = b ^ *(number);
+  b = b ^ *(number + 1);
+  b = b ^ *(number + 2);
+  b = b ^ *(number + 3);
+  b = b ^ *(number + 4);
+  b = b ^ *(number + 5);
+  b = b ^ *(number + 6);
+  b = b ^ *(number + 7);
 
   return ((~b) & 1);
-}
 
+}
 
 int main (int argc, char* argv[])
 {
@@ -36,6 +40,7 @@ int main (int argc, char* argv[])
   initializePipes();
 
   pid = fork();
+
   if (pid == -1)
   {
     perror("Fork error\n");
@@ -43,35 +48,32 @@ int main (int argc, char* argv[])
   }
   else if (pid == 0)
   {
-    childProcess();
+    childProcess(argv[1]);
   }
   else
   {
-    parentProcess();
+    parentProcess(pipeCtoP, pipePtoC, BUFFER_SIZE);
+    wait(NULL);
   }
   return 0;
 
 }
 
-/*
-*
-*
-*/
-
 static void childProcess(char * command)
 {
   char *args[4];
-  close(stdinFD);
-  close(stdoutFD);
 
-  close(pipeCtoP[0]);
-  close(pipePtoC[1]);
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+
+  close(pipeCtoP[READ]);
+  close(pipePtoC[WRITE]);
 
 // redirects standard input to child's end pipe
-  dup2(pipePtoC[0], fileno(stdin));
+  dup2(pipePtoC[READ], STDIN_FILENO);
 
 // redirects parent's to standard output  pipeCtoP[1], fileno(stdout)
-  dup2(pipeCtoP[1], fileno(stdout));
+  dup2(pipeCtoP[WRITE], STDOUT_FILENO);
 
   args[0] = "bash";
   args[1] = "-c";
@@ -81,12 +83,35 @@ static void childProcess(char * command)
   execvp(args[0], args);
 }
 
-int parentProcess()
+void parentProcess(int pipeCtoP[], int pipePtoC[], int bufferSize)
 {
-  close (pipePtoC[0]);
-  close (pipeCtoP[1]);
+  int inputParity = 0;
+  int outputParity = 0;
+  char buffer[bufferSize];
+  int bytesRead = 0;
 
+  close(pipeCtoP[WRITE]);
+  close(pipePtoC[READ]);
 
+  while((bytesRead = read(STDIN_FILENO, buffer, bufferSize - 1)) > 0){
+    buffer[bytesRead] = 0;
+    inputParity ^= bitParity(buffer);
+    write(pipePtoC[WRITE], buffer, bytesRead);
+  }
+
+  close(pipePtoC[WRITE]);
+
+  while((bytesRead = read(pipeCtoP[READ], buffer, bufferSize - 1)) > 0){
+    buffer[bytesRead] = 0;
+    outputParity ^= bitParity(buffer);
+    printf("%s ",buffer);
+  }
+
+  outputParity = outputParity ^ '\n';
+
+  /* %02 imprime al menos 2 digitos, si hay menos de 2 digitos pone un 0 adelante */
+  fprintf(stderr, "in parity: 0x%02X\n", inputParity);
+  fprintf(stderr, "out parity: 0x%02X\n", outputParity);
 
 }
 
---
+++ ej1.h
@@ -0,0 +1,17 @@
+#ifndef EJ2_H
+#define EJ2_H
+
+#define READ 0
+#define WRITE 1
+/*1024 - 512 - 2048 */
+#define BUFFER_SIZE 1024
+
+int bitParity(char * number);
+
+static void childProcess(char * command);
+
+void parentProcess(int pipeCtoP[], int pipePtoC[], int bufferSize);
+
+void initializePipes();
+
+#endif
